e4ac18d1ce5f003d4cc4b974d10981511bdad0ac
Luks245 <florekl03c@lo5.bielsko.pl>
2023-06-08 18:21:46 +0200

    added projects page

--- global.js
+++ global.js
@@ -1,33 +1,78 @@
-
-function showMenu(){
-    document.getElementById('nav-list').classList.toggle('view')
-  }
-  
-  function activePage(e){
-    let children = document.getElementById('nav-list').children
-    for(let i = 0; i< children.length; i++){
-        children[i].classList.remove('active')
-    }
-    e.target.classList.add('active')
-  }
-
-const hoverEffect = e =>{
-  for(const card of document.querySelectorAll('.skill-card')){
-      const rect = card.getBoundingClientRect(),
-          x = e.clientX-rect.left,
-          y = e.clientY-rect.top;
-
-      card.style.setProperty("--mouse-x",`${x}px`);
-      card.style.setProperty("--mouse-y",`${y}px`);
-  }
+function showMenu() {
+  document.getElementById("nav-list").classList.toggle("view");
 }
+
+function activePage(e) {
+  let children = document.getElementById("nav-list").children;
+  for (let i = 0; i < children.length; i++) {
+    children[i].classList.remove("active");
+  }
+  e.target.classList.add("active");
+}
+
+const hoverEffect = (e) => {
+  for (const card of document.querySelectorAll(".skill-card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
 
 window.onload = () => {
-  const containerSkills = document.getElementById('skills-container') ? document.getElementById('skills-container') : ''
-  const containerAbout = document.getElementById('about-main') ? document.getElementById('about-main') : ''
-  if(containerSkills !== '')
-    containerSkills.classList.add('animate')
-  if(containerAbout !== '')
-    containerAbout.classList.add('animate')
-}
+  const containerSkills = document.getElementById("skills-container")
+    ? document.getElementById("skills-container")
+    : "";
+  const containerAbout = document.getElementById("about-main")
+    ? document.getElementById("about-main")
+    : "";
+  if (containerSkills !== "") containerSkills.classList.add("animate");
+  if (containerAbout !== "") containerAbout.classList.add("animate");
+};
 
+const gallery = document.getElementById("gallery");
+let counter = 0;
+// let mousedown = false;
+// let mouseStartPosition = 0;
+
+const slideLeft = () => {
+  if (counter > 3) {
+    gallery.style.transition = "none";
+    gallery.style.transform = "translateX(0%)";
+
+    counter = 0;
+  }
+  counter++;
+
+  setTimeout(() => {
+    gallery.style.transform = `translateX(-${counter * 100}vw)`;
+    gallery.style.transition = "transform 0.5s";
+  }, 10);
+};
+
+const slideRight = () => {
+  if (counter <= 0) {
+    gallery.style.transition = "none";
+    gallery.style.transform = "translateX(-400vw)";
+
+    counter = 4;
+  }
+  counter--;
+
+  setTimeout(() => {
+    gallery.style.transform = `translateX(-${counter * 100}vw)`;
+    gallery.style.transition = "transform 0.5s";
+  }, 10);
+};
+
+// window.onmousedown = (e) => {
+//   mousedown = true;
+//   mouseStartPosition = e.clientX;
+// };
+
+// window.onmouseup = (e) => {
+//   if (e.clientX - mouseStartPosition > 200) slideRight();
+//   else if (e.clientX - mouseStartPosition < -200) slideLeft();
+// };
--- three_js.js
+++ three_js.js
@@ -1,88 +1,96 @@
-import * as THREE from 'three'
-import './style.css'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import * as THREE from "three";
+import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const scene = new THREE.Scene()
-scene.background = null
+const scene = new THREE.Scene();
+scene.background = null;
 
 const sizes = {
-    width: window.innerWidth/2,
-    height:window.innerHeight-80
-}
+  width: window.innerWidth / 2,
+  height: window.innerHeight - 80,
+};
 
-const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set(10, 10, 10)
-scene.add(light)
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(10, 10, 10);
+scene.add(light);
 
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
 
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
-camera.position.z = 20
-scene.add(camera)
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 2;
 
-const canvas = document.querySelector('.webgl')
-const renderer = new THREE.WebGLRenderer({canvas, alpha:true})
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(2)
-renderer.render(scene,camera)
-
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-controls.enablePan = false
-controls.enableZoom = false
-controls.autoRotate = true
-controls.autoRotateSpeed = 2
-
-const loadingManager = new THREE.LoadingManager()
-const progressBar = document.querySelector('#progress-bar')
-const loaderDiv = document.querySelector('.loader')
-const navbar = document.querySelector('#nav-home')
-const mainHome = document.querySelector('#main-home')
+const loadingManager = new THREE.LoadingManager();
+const progressBar = document.querySelector("#progress-bar");
+const loaderDiv = document.querySelector(".loader");
+const navbar = document.querySelector("#nav-home");
+const mainHome = document.querySelector("#main-home");
 
 window.onload = () => {
-  navbar.style.visibility = "hidden"
-  mainHome.style.visibility = "hidden"
-}
+  navbar.style.visibility = "hidden";
+  mainHome.style.visibility = "hidden";
+};
 
-loadingManager.onProgress = (url,loaded,total) => {
-  progressBar.value = (loaded/total)*100
-}
+loadingManager.onProgress = (url, loaded, total) => {
+  progressBar.value = (loaded / total) * 100;
+};
 loadingManager.onLoad = () => {
-  loaderDiv.style.display = 'none'
-  navbar.style.visibility = "visible"
-  mainHome.style.visibility = "visible"
-  mainHome.classList.add('animate')
-}
+  loaderDiv.style.display = "none";
+  navbar.style.visibility = "visible";
+  mainHome.style.visibility = "visible";
+  mainHome.classList.add("animate");
+};
 
-let loader = new GLTFLoader(loadingManager)
-loader.load('./assets/earth.glb', gltf => {
-    scene.add(gltf.scene)
-})
+let loader = new GLTFLoader(loadingManager);
+loader.load("./assets/earth.glb", (gltf) => {
+  scene.add(gltf.scene);
+});
 
-
-window.addEventListener('resize', () => {
-  if(window.innerHeight<600) return
-    sizes.width = window.innerWidth > 1120 ? window.innerWidth/2 : window.innerWidth
-    sizes.height = window.innerWidth > 1120 ? window.innerHeight-80 : window.innerHeight-270
-    camera.updateProjectionMatrix()
-    camera.aspect = sizes.width/sizes.height
-    renderer.setSize(sizes.width, sizes.height)
-})
+window.addEventListener("resize", () => {
+  if (window.innerHeight < 600) return;
+  sizes.width =
+    window.innerWidth > 1120 ? window.innerWidth / 2 : window.innerWidth;
+  sizes.height =
+    window.innerWidth > 1120
+      ? window.innerHeight - 80
+      : window.innerHeight - 270;
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+});
 setInterval(() => {
-  if(window.innerHeight<600) return
-  sizes.width = window.innerWidth > 1120 ? window.innerWidth/2 : window.innerWidth
-    sizes.height = window.innerWidth > 1120 ? window.innerHeight-80 : window.innerHeight-270
-    camera.updateProjectionMatrix()
-    camera.aspect = sizes.width/sizes.height
-    renderer.setSize(sizes.width, sizes.height)
-},10)
+  if (window.innerHeight < 600) return;
+  sizes.width =
+    window.innerWidth > 1120 ? window.innerWidth / 2 : window.innerWidth;
+  sizes.height =
+    window.innerWidth > 1120
+      ? window.innerHeight - 80
+      : window.innerHeight - 270;
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+}, 10);
 
 const loop = () => {
-    controls.update()
-    renderer.render(scene,camera)
-    window.requestAnimationFrame(loop)
-}
-loop()
-
-
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
