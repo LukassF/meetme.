9c99c31f0362a3ed99d94faa6415c41cd398903a
Luks245 <florekl03c@lo5.bielsko.pl>
2023-08-04 14:21:25 +0200

    added gsap animations

--- public/global.js
+++ public/global.js
@@ -1,7 +1,14 @@
 gsap.registerPlugin(scrollTo);
+gsap.registerPlugin(ScrollTrigger);
 
-function showMenu() {
+function showMenu(e) {
+  e.target.classList.toggle("shown");
   document.getElementById("nav-list").classList.toggle("view");
+}
+
+function navigateTo(href, e) {
+  if (e.target.localName === "i" || e.target.localName === "a") return;
+  window.open(href, "_blank").focus();
 }
 
 let navLinks = [...document.getElementById("nav-list").children];
@@ -11,7 +18,6 @@ function activePage(e) {
     link.children[0].classList.remove("active");
   });
   e.target.classList.add("active");
-  console.log(e.target.dataset);
 
   switch (e.target.dataset.value) {
     case "home":
@@ -39,37 +45,12 @@ function activePage(e) {
 
 const hoverEffect = (e) => {
   for (const card of document.querySelectorAll(".skill-card")) {
-    const child = e.target.children[0].children[0];
-
-    card.children[0].children[0].style.transform = "none";
-    // console.log(child.classList.length);
     const rect = card.getBoundingClientRect(),
       x = e.clientX - rect.left,
       y = e.clientY - rect.top;
 
-    const currentRect = e.target.getBoundingClientRect();
-
     card.style.setProperty("--mouse-x", `${x}px`);
     card.style.setProperty("--mouse-y", `${y}px`);
-
-    console.log(
-      ((e.clientX - currentRect.left - card.clientWidth / 2) /
-        card.clientWidth) *
-        200
-    );
-
-    // if (child.classList.length === 0)
-    //   child.style.transform = `translate(${
-    //     -(
-    //       (e.clientX - currentRect.left - card.clientWidth / 2) /
-    //       card.clientWidth
-    //     ) * 10
-    //   }%, ${
-    //     -(
-    //       (e.clientY - currentRect.top - card.clientHeight / 2) /
-    //       card.clientHeight
-    //     ) * 10
-    //   }%)`;
   }
 };
 
@@ -78,31 +59,16 @@ const traits = (e, value) => {
   e.target.parentElement.children[2].classList.toggle("show", value);
 };
 
-// const showResume = () => {
-//   const resumeList = document.getElementById("resume-list");
-//   resumeList.style.visibility = "visible";
-//   resumeList.style.opacity = "1";
-// };
-// const hideResume = () => {
-//   const resumeList = document.getElementById("resume-list");
-//   resumeList.style.opacity = "0";
-//   setTimeout(() => {
-//     resumeList.style.visibility = "hidden";
-//   }, 400);
-// };
-
 const containers = [...document.getElementsByTagName("main")];
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      console.log(entry.target.dataset.navlink);
       navLinks.forEach((link) => {
         if (link.innerText === entry.target.dataset.navlink)
           link.children[0].classList.add("active");
         else link.children[0].classList.remove("active");
       });
-      // entry.target.classList.toggle("show", entry.isIntersecting);
     });
   },
   {
@@ -112,6 +78,28 @@ const observer = new IntersectionObserver(
 
 containers.forEach((container) => {
   observer.observe(container);
+});
+
+const projects = [...document.getElementsByClassName("phone-container")];
+
+const observerProjects = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (window.innerWidth < 850) {
+        entry.target.classList.toggle("show", entry.isIntersecting);
+        entry.target.style.transition = "all 0.3s";
+      } else {
+        entry.target.style.transition = "none";
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+projects.forEach((project) => {
+  observerProjects.observe(project);
 });
 
 //checking if the gifs are loaded
@@ -138,3 +126,154 @@ intervalID = setInterval(() => {
     if (projectsLoader !== "") projectsLoader.classList.add("fadeOut");
   }
 });
+
+//GSAP animations
+
+gsap.to(".home-background", {
+  scrollTrigger: {
+    trigger: ".home-background",
+    scrub: 1,
+  },
+  yPercent: -30,
+  opacity: 0.2,
+});
+
+gsap.to(".skills-background", {
+  scrollTrigger: {
+    trigger: ".skills-background",
+    scrub: 1,
+  },
+  yPercent: -30,
+  opacity: 0.1,
+});
+
+gsap.from(".home-el", {
+  scrollTrigger: {
+    trigger: "#main-home",
+    toggleActions: "restart reset restart pause ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+  delay: 0.6,
+  x: -300,
+  opacity: 0,
+  duration: 0.3,
+  ease: "elastic",
+});
+
+gsap.from(".social-link", {
+  scrollTrigger: {
+    trigger: "#main-home",
+    toggleActions: "restart reset restart pause ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+  delay: 1,
+  yPercent: 30,
+  opacity: 0,
+  stagger: 0.1,
+  duration: 0.3,
+});
+
+gsap.from("#images-home", {
+  scrollTrigger: {
+    trigger: "#main-home",
+    toggleActions: "restart reset restart pause ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+  delay: 0.3,
+  scale: 0,
+  opacity: 0,
+  duration: 0.3,
+});
+
+const frameTl = gsap.timeline({
+  defaults: { duration: 0.4 },
+  scrollTrigger: {
+    trigger: "#about-main",
+    toggleActions: "restart none none pause ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+});
+
+frameTl.to(".frame-about-top", { scaleX: 1 });
+frameTl.to(".frame-about-left", { scaleY: 1 });
+frameTl.to(".frame-about-bottom", { scaleX: 1 });
+frameTl.to(".frame-about-right", { scaleY: 1 });
+
+gsap.from(".about-el", {
+  scrollTrigger: {
+    trigger: "#about-main",
+    toggleActions: "restart none none reset ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+
+  xPercent: 10,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.3,
+});
+
+gsap.to(".h-line", {
+  scrollTrigger: {
+    trigger: "#skills-main",
+    toggleActions: "restart none none reset ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+  scaleX: 1,
+  duration: 1.2,
+});
+
+gsap.from(".skill-card", {
+  scrollTrigger: {
+    trigger: "#skills-main",
+    toggleActions: "restart none none reset ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+  xPercent: -10,
+  duration: 0.3,
+  stagger: 0.1,
+});
+
+gsap.from(".skill-card-img", {
+  scrollTrigger: {
+    trigger: "#skills-main",
+    toggleActions: "restart none none reset ",
+    start: "20% 80%",
+    end: "80% 20%",
+  },
+  yPercent: -20,
+  opacity: 0.6,
+  duration: 0.3,
+  stagger: 0.1,
+  delay: 0.4,
+});
+gsap.from(".projects-h1", {
+  scrollTrigger: {
+    trigger: "#projects-main",
+    toggleActions: "restart none none reset ",
+  },
+  xPercent: -15,
+  opacity: 0,
+  duration: 0.5,
+  ease: "back",
+});
+if (window.innerWidth > 850)
+  gsap.from(".phone-container", {
+    scrollTrigger: {
+      trigger: "#projects-main",
+      toggleActions: "restart none none reset ",
+      start: "20% 80%",
+      end: "80% 20%",
+    },
+    xPercent: -15,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.1,
+    ease: "back",
+  });
